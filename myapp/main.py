9775ac2ba81b18c9d8a6aaf8de90e742cece1913
eunbee <eunbee1224@gmail.com>
2020-04-26 22:14:19 +0900

    <feature>: web-scraping (wbiz board:board)

---
+++ myapp/main.py
@@ -0,0 +1,5 @@
+from wbiz import extract_wbiz_pages, extract_wbiz_board
+
+last_wbiz_page = extract_wbiz_pages()
+
+wbiz_boards= extract_wbiz_board(last_wbiz_page)
---
+++ myapp/wbiz.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"http://www.wbiz.or.kr/web/bizinfo/BD_index.do?q_searchKey=&q_searchVal=&q_mcategory_type=1&q_rowPerPage={LIMIT}"
+
+def extract_wbiz_pages():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class":"paginate"})
+
+    links = pagination.find_all('a')
+    pages = []
+    for link in links:
+        pages.append(int(link.string))
+
+    max_page = pages[-1]
+
+    return max_page
+
+def extract_wbiz_board(last_page):
+    for page in range(last_page):
+        boards = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(boards.text, "html.parser")
+        table_datas = soup.find_all("td", {"class":"tx_l"})
+
+        for data in table_datas:
+            content = data.find("a", {"class":"contentTip"})
+            if content is not None:
+                print(content.string.strip())
+
