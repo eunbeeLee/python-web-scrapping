139a4a8fc9edd26b088739f19cfe639d8e3c1600
eunbee <eunbee1224@gmail.com>
2020-05-05 22:56:32 +0900

    <feature>: web-scraping-wbiz(serveral categories) complete

--- myapp/wbiz.py
+++ myapp/wbiz.py
@@ -1,5 +1,4 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
 LIMIT = 50
@@ -11,48 +10,66 @@ MARKETING_EXPORTING_URL = f"http://www.wbiz.or.kr/web/bizinfo/BD_index.do?q_mcat
 
 def get_pages_by_category():
     urls = [STARTING_BUSINESS_URL, CAPITAL_URL, LEARNING_CENTER_URL, MARKETING_EXPORTING_URL]
-    extract_board(urls)
+    return get_boards(urls)
 
-def extract_board(urls):
+def get_boards(urls):
     boards = []
-    for url in urls:
+    for index, url in enumerate(urls):
         soup = get_soup(url)
         last_page = get_last_page(soup)
-        boards.append(extract_wbiz_board(last_page, url))
+        boards.append(extract_wbiz_board(last_page, url, index))
+    return boards
 
-def get_soup(URL):
-    result = requests.get(URL)
+def get_soup(url):
+    result = requests.get(url)
     return BeautifulSoup(result.text, "html.parser")
 
 def get_last_page(soup):
-    pagination = soup.find("div", {"class":"pagination"})
+    pagination = soup.find("div", {"class":"paginate"})
     page_links = pagination.find_all('a')
 
     pages = []
     for link in page_links:
-        pages.append(int(link.string))
+        title = link.attrs['title']
+        if title != '다음페이지로 가기' and title != '마지막페이지로 가기':
+            pages.append(int(link.string))
     return pages[-1]
 
-def extract_board(content, url):
-    title = content.string.strip()
-    link = content.attrs["onclick"].split(";")[0].split("'")[1]
-    parent = content.parent
-    # parent_content = parent.attrs["contents"]
-    return {"title": title, "link": url + f"&bbsCd={link}"}
 
-def extract_wbiz_board(last_page, url):
+
+def extract_wbiz_board(last_page, url, index):
+    board = []
     for page in range(last_page):
-        boards = requests.get(f"{URL}&start={page*LIMIT}")
+        boards = requests.get(f"{url}&start={page*LIMIT}")
         soup = BeautifulSoup(boards.text, "html.parser")
-        table_datas = soup.find_all("td", {"class":"tx_l"})
+        tr_data = soup.find('tbody').find_all("tr")
 
-        board = []
+        category = get_category(index)
+        for tr in tr_data:
+            td_data = tr.find_all("td")
+            if td_data[0].text == '':
+                continue
+            else:
+                board.append({"category": category, "content": extract_information(td_data, url)})
 
-        for data in table_datas:
-            content = data.find("a", {"class":"contentTip"})
-            if content is not None:
-                board.append(extract_board(content, url))
-        return board
+    return board
+
+def get_category(index):
+    if index == 0:
+        return '창업/보육'
+    if index == 1:
+        return '자금'
+    if index == 2:
+        return '교육/일자리'
+    if index == 3:
+        return '수출'
+
+def extract_information(td_data, url):
+    title = td_data[1].string.strip()
+    link = td_data[1].next.attrs['onclick'].split(";")[0].split("'")[1]
+    team = td_data[2].text
+    date = td_data[3].text
+    return {'title': title, 'link': url + f'&bbsCd={link}', 'team': team, 'date': date}
 
 
 
